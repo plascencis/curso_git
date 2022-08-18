00a5397adfe27b3d55b16e750bc9ec61db98132a
jesus <jesus.plascencia@m3eng.com>
2022-08-18 08:40:35 -0700

    parrafo y tamaño de fuente

--- index.php
+++ index.php
@@ -3,8 +3,9 @@
 <head>
 <meta charset="utf-8">
 <title>Curso Git</title>
+<link href="css/estilos.css" rel="stylesheet" type="text/css">
 </head>
 <body>
-
+<h1>Proyecto empresarial</h1>
 </body>
 </html>
