6ece5836f15759d550793ce49ea06e5813d70fb9
jesus <jesus.plascencia@m3eng.com>
2022-08-18 15:14:09 -0700

    saludo javascript agregado

--- index.php
+++ index.php
@@ -4,9 +4,15 @@
 <meta charset="utf-8">
 <title>Curso Git</title>
 <link href="css/estilos.css" rel="stylesheet" type="text/css">
+
+<script src="js/javascript.js"></script>
 </head>
+
 <body>
 <h1>Proyecto empresarial</h1>  
   <p>Párrafo agregado</p>
+
+  <input type="button" onClick="saludo()" value="Dale ya!">
+
 </body>
 </html>
